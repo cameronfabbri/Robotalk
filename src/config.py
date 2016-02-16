6819deffd13263f393cd5b533f64266d876611df
Cameron Fabbri <cameronfabbri@gmail.com>
2016-02-15 21:38:09 -0500

    removed class, added functions, and started ability for built in commands

--- src/config.py
+++ src/config.py
@@ -15,7 +15,12 @@ entrance_label = 0
 exit_label     = 1
 learn_label    = 2
 
-confidence_threshold = 0.6
+confidence_threshold = 0.75
+
+built_in = [
+   ('new commad'),
+   ('goodbye')
+]
 
 train = [
    ('hello how are you today', 0),
@@ -24,6 +29,7 @@ train = [
    ('hey how is your day', 0),
    ('good morning', 0),
    ('good night', 1),
+   ('i\'m going home', 1),
    ('have a good day', 1),
    ('see you later', 1),
    ('i want to teach you something', 2),
@@ -42,39 +48,3 @@ train = [
    ('bring this letter to the mail room down the hall', 3),
    ('deliver this letter to room 365', 3)
 ]
-
-
-"""
-train = [
-   ('hey', 0),
-   ('hello', 0),
-   ('hi', 0),
-   ('what\'s up', 0),
-   ('yo', 0),
-   ('how\'s it going?', 0),
-   ('what\'s new?', 0),
-   ('hey how is your day', 0),
-   ('good morning', 0),
-   ('howdy', 0),
-   ('good night', 1),
-   ('cya', 1),
-   ('goodbye', 1),
-   ('bye', 1),
-   ('later', 1),
-   ('have a good day', 1),
-   ('farewell', 1),
-   ('take care', 1),
-   ('bye bye', 1),
-   ('see you later', 1),
-   ('i want to teach you something', 2),
-   ('time to learn', 2),
-   ('want to learn something new?', 2),
-   ('new command', 2),
-   ('learn new command', 2),
-   ('new command to learn', 2),
-   ('i\'ve got a new command for you', 2),
-   ('here\'s something new' ,2),
-   ('remember this', 2),
-   ('you should know this', 2)
-]
-"""
--- src/parser.py
+++ src/parser.py
@@ -5,7 +5,6 @@ import operator
 import cPickle as pickle
 import time
 import sys
- 
 # Your configuration file(s)
 import config
 
@@ -29,97 +28,108 @@ loop through the possible commands
 
 """
 
-class Parser(object):
-   def __init__(self, command):
-      self.command = command
+if len(sys.argv) < 2:
+   print "Usage: python parser.py [classifier.file]"
+   exit(-1)
+classifier_file = sys.argv[1]
 
-   if len(sys.argv) < 2:
-      print "Usage: python parser.py [classifier.file]"
-      exit(-1)
-   classifier_file = sys.argv[1]
+try:
+   cll = pickle.load(open(classifier_file, 'rb'))  
+   print "Loaded classifier"
+except:
+   print "No classifier found or file corrupted! Run setup.py first to train a base classifier"
+   exit(-1)
 
-   try:
-      cll = pickle.load(open(classifier_file, 'rb'))  
-      print "Loaded classifier"
-   except:
-      print "No classifier found or file corrupted! Run setup.py first to train a base classifier"
-      exit(-1)
+def learn_new_command(command, labels):
+   print "Okay learning a new command...\n"
+   # get last label used
+   last_label = labels[::-1]
+   ll = last_label[0]         
+   new_label = ll + 1
+   print "new label: " + str(new_label)
+   new_command = raw_input("New Command: ")
+   new_data = [(new_command, new_label)]
+   cll.update(new_data)
+   print "Saving new classifier..."
+   f = open(classifier_file, 'wb')
+   pickle.dump(cll, f)
+   return -1
 
-   """
-      Parses the command given, returns a json blob of possible location, object, subject, etc
-   """
-   def parseCommand(command, cll, classifier_file):
-      confidence_threshold = config.confidence_threshold
-      entrance_label       = config.entrance_label
-      learn_label          = config.learn_label
-      exit_label           = config.exit_label
-      prob_dist            = cll.prob_classify(command)
-      labels               = cll.labels()
+def update_classifier(cll, prob_label_dict):
+   print "I'm not sure what you mean...\n"
+   l = raw_input("Please give me an example command for which this falls into\n")
 
-      # most probable label.
-      mpl  = 0 
-      # This will print out the label it thinks it is
-      #blob = TextBlob(command, classifier=cll)
-      #print blob.classify()
+   # This returns a label
+   ll = TextBlob(l, classifier=cll).classify()
 
-      # I want to find the probility of each label, and pick the highest one.
-      # if the highest one is below say 0.60, then ask for confirmation.
-      # after confirmation, if any, update model
+   # find the probabilities of the commands, sort them, then ask in descending order
+   prob_label_dict = sorted(prob_label_dict.items(), key=operator.itemgetter(1))[::-1]
+   prob_label_list = list(prob_label_dict)
+   
+   new_label = prob_label_list[0][0]
+   print "Adding command " + str(command) + " with label " + str(new_label)
 
-      # this contains: dict[label] = probability
-      prob_label_dict = dict()
-      for label in labels:
-         if prob_dist.prob(label) > prob_dist.prob(mpl):
-            mpl = label
-         print "Probability for label " + str(label) + ": " + str(prob_dist.prob(label))
-         prob_label_dict[label] = prob_dist.prob(label)
-      print "Most probable label: " + str(mpl)          
+   new_data = [(command, new_label)]
+   cll.update(new_data)
+   f = open(classifier_file, 'wb')
+   pickle.dump(cll, f)
+   return -1
 
-      if mpl == exit_label and prob_dist.prob(mpl) > confidence_threshold:
-         print "Goodbye!"
-         exit()      
+def isBuiltIn(command):
+   built_in = config.built_in
+   print built_in
+   for b_command in built_in:
+      if b_command == command:
+         print "Built in command " + str(command)
+         exit()
 
-      # This is for learning a brand new command / label
-      if mpl == learn_label and prob_dist > confidence_threshold:
-         print "Okay learning a new command...\n"
-         # get last label used
-         last_label = labels[::-1]
-         ll = last_label[0]         
-         new_label = ll + 1
-         print "new label: " + str(new_label)
-         new_command = raw_input("New Command: ")
-         new_data = [(new_command, new_label)]
-         cll.update(new_data)
-         print "Saving new classifier..."
-         f = open(classifier_file, 'wb')
-         pickle.dump(cll, f)
+"""
+   Parses the command given, returns a json blob of possible location, object, subject, etc
+"""
+def parseCommand(command, cll, classifier_file):
+   confidence_threshold = config.confidence_threshold
+   entrance_label       = config.entrance_label
+   learn_label          = config.learn_label
+   exit_label           = config.exit_label
+   prob_dist            = cll.prob_classify(command)
+   labels               = cll.labels()
+   prob_label_dict      = dict()
 
-      # this is if the threshold wasn't passed. Add more to knowledge
-      if prob_dist.prob(mpl) < confidence_threshold:
-         print "I'm not sure what you mean...\n"
-         l = raw_input("Please give me an example command for which this falls into\n")
+   # most probable label.
+   mpl  = 0 
+   # This will print out the label it thinks it is
+   #blob = TextBlob(command, classifier=cll)
+   #print blob.classify()
 
-         # This returns a label
-         ll = TextBlob(l, classifier=cll).classify()
+   # before using the classifier, check if it is a built in command
+   isBuiltIn(command)
 
-         # find the probabilities of the commands, sort them, then ask in descending order
-         prob_label_dict = sorted(prob_label_dict.items(), key=operator.itemgetter(1))[::-1]
-         print prob_label_dict
+   for label in labels:
+      if prob_dist.prob(label) > prob_dist.prob(mpl):
+         mpl = label
+      print "Probability for label " + str(label) + ": " + str(prob_dist.prob(label))
+      prob_label_dict[label] = prob_dist.prob(label)
+   print "Most probable label: " + str(mpl)          
 
-         # so commands need to be multiple words (or should be, don't HAVE to be)
-         # let's have the output be the label of the command, so all the user needs
-         # to do is say in their code if label == x do this. That'll be the mapping
-         # between here and there
+   # Maybe for short commands just have them built in
+   if mpl == exit_label and prob_dist.prob(mpl) > confidence_threshold:
+      print "Goodbye!"
 
-         #for label, prob in prob_label_dict.iteritems():
-         #   print "label: " + str(label) + " -> " + str(prob)
-  
-         #print "Adding command " + str(command) + " with label " + str(ll)
-         #new_data = [(command, ll)]
-         #cll.update(new_data)
+   # This is for learning a brand new command / label
+   if mpl == learn_label and prob_dist.prob(mpl) > confidence_threshold:
+      learn_new_command(command, labels)
 
+   # this is if the threshold wasn't passed. Add more to knowledge
+   if prob_dist.prob(mpl) < confidence_threshold:
+      update_classifier(cll, prob_label_dict)
 
-   while True:
-      command = raw_input("> ")
-      s = time.time()
-      parseCommand(command, cll, classifier_file)
+   return mpl
+
+while True:
+   command = raw_input("> ")
+   s = time.time()
+
+   # This return label is what is sent to the robot
+   # The developer has to link this label with their functions
+   return_label = parseCommand(command, cll, classifier_file)
+   print "return label: " + str(return_label)
