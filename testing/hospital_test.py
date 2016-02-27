07246847653e6c38365c817b416df4a218d97142
Cameron Fabbri <fabbricm@clarkson.edu>
2016-02-27 01:21:09 +0000

    testing update

--- testing/hospital_test.py
+++ testing/hospital_test.py
@@ -70,8 +70,8 @@ for obj in test:
    true_labels.append(obj[1])
 
 
-print "pred: ", pred_labels
-print "true: ", true_labels
+#print "pred: ", pred_labels
+#print "true: ", true_labels
 #confusion_matrix(y_true, y_pred)
 print confusion_matrix(true_labels, pred_labels)
 
