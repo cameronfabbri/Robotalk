35fd74769e2c90d11300058365bb21abd6832858
Cameron Fabbri <cameronfabbri@gmail.com>
2016-04-01 04:56:39 +0000

    put scan ports in py_connect

--- dev/py_connect.py
+++ dev/py_connect.py
@@ -1,6 +1,16 @@
 import serial
-port = '/dev/ttyACM0'
+import glob
+
+def scan():
+   """scan for available ports. return a list of device names."""
+   return glob.glob('/dev/ttyS*') + glob.glob('/dev/ttyUSB*') + glob.glob('/dev/ttyACM*')
+
+
+port = scan()[0]
+print port
 
 ser = serial.Serial(port, 9600)
 
 ser.write(b'1')
+print "Wrote..."
+
